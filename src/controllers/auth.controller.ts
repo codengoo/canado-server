ac88067f87e4ff7ab2ed75715efbb75b46eaac75
NghiaDT <nghiadt22.work@gmail.com>
2024-11-24 16:40:27 +0700

    patch

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,8 +6,11 @@ import ValidationError from '../utils/valError';
 
 export default class AuthController {
   static async openDeepLink(req: Request, res: Response) {
+    // patch: Remove on production
+    const id = req.params.id;
+
     await handleExceptions(res, async () => {
-      const user = req.user as IUser;
+      const user = (req.user as IUser) || { id };
       if (!user) throw new ValidationError('Invalid user');
       const token = jwt.sign({ sub: user.id }, process.env.JWT_TOKEN);
       const refresh = jwt.sign({ token }, process.env.JWT_REFRESH);
