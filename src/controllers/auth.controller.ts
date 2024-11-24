7b9388af20bdea8a9b61aa5499f5d3a36a405bd9
NghiaDT <nghiadt22.work@gmail.com>
2024-11-24 16:48:48 +0700

    done

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,19 +1,25 @@
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { IUser } from '../data';
+import UserModel from '../models/user.model';
 import { handleExceptions } from '../utils';
 import ValidationError from '../utils/valError';
 
 export default class AuthController {
   static async openDeepLink(req: Request, res: Response) {
     // patch: Remove on production
-    const id = req.params.id;
+    const id = req.query.id as string;
 
     await handleExceptions(res, async () => {
-      const user = (req.user as IUser) || { id };
+      // const user = req.user as IUser;
+      
+      // TODO: remove on product
+      const user =
+        (req.user as IUser) || (id && (await UserModel.getUserByID(id)));
+
       if (!user) throw new ValidationError('Invalid user');
-      const token = jwt.sign({ sub: user.id }, process.env.JWT_TOKEN);
-      const refresh = jwt.sign({ token }, process.env.JWT_REFRESH);
+      const token = jwt.sign(user, process.env.JWT_TOKEN);
+      const refresh = jwt.sign(user, process.env.JWT_REFRESH);
       res.redirect(`nacado://auth?token=${token}&refresh=${refresh}`);
     });
   }
